6e567e534e5d7d15d8be1b7a9895b2674a01407b
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-06-10 12:44:41 +0200

    init sass

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './styles.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
