97c0ef86e2517526195321755d54111ee908adf1
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-06-25 23:32:47 +0200

    center nav buttons in a btn-container

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,27 +1,41 @@
 <template>
     <header class="header">
-        <nav class="navbar">
-            <img alt="logo" src="/src/assets/styles/img/logo/logo-nav-768-2x.webp">
-        </nav>
         <CountdownBar />
+        <nav class="navbar">
+            <img class="img" alt="logo" src="../../assets/styles/img/logo/logo-nav-768-2x.webp">
+            <NavBtn />
+        </nav>
     </header>
 </template>
 
 <script setup>
 import CountdownBar from '@/components/CountdownBar.vue';
+import NavBtn from '@/components/navbar/NavBtn.vue';
 </script>
 
 <style lang="scss" scoped>
 @import '/src/assets/styles/styles.scss';
+
 .header {
-    //left: 0;
+    left: 0;
     position: fixed;
     top: 0;
     width: 100vw;
 
     .navbar {
         background-color: $navbar-background;
-    }
+        height: 176px;
 
+    }
+    
+    .img {
+        display: block;
+        position: relative;
+        top: 6rem;
+        height: auto;
+        max-width: 40%;
+        max-height: 40%;
+        padding-left: 160px;
+    }
 }
 </style>
---
+++ src/components/navbar/NavBtn.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="btn-container">
+      <button 
+        class="nav-btn" 
+        v-for="(button, index) in buttons"
+        :key="index"
+      >
+        {{ button.label }}
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  const buttons = [
+    { label: 'Home' },
+    { label: 'General Information' },
+    { label: 'Exhibitor' },
+    { label: 'Specialist Program' },
+    { label: 'Advisory Board' },
+    { label: 'Request Stand' },
+  ];
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '/src/assets/styles/_variables.scss';
+  
+  .btn-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    margin-top: 20px;
+  
+    .nav-btn {
+      height: 36px;
+      padding: 5px 10px;
+      background-color: $countdown-background;
+      color: white;
+      border: none;
+      border-radius: 16px;
+      cursor: pointer;
+      margin: 5px;
+    }
+}
+  </style>
+  
